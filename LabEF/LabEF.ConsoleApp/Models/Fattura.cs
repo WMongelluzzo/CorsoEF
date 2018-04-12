69a4e9f80f5d739df5badc4f85ee70777258e9de
Walter <walter.mongelluzzo@gmail.com>
2018-04-12 11:38:03 +0200

    Creata classe Fattura

---
+++ LabEF/LabEF.ConsoleApp/Models/Fattura.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabEF.ConsoleApp.Models
+{
+    class Fattura
+    {
+        public int FatturaId { get; set; }
+        public DateTime DataFattura { get; set; }
+        public string NumeroFattura { get; set; }
+    }
+}
