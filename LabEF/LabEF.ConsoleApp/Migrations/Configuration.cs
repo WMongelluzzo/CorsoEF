b4edd1db66ee0c9a593684c6fcc5732266be8127
Walter <walter.mongelluzzo@gmail.com>
2018-04-12 16:28:46 +0200

    fixata connesione a DB

---
+++ LabEF/LabEF.ConsoleApp/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+namespace LabEF.ConsoleApp.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<LabEF.ConsoleApp.Data.FatturarioContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(LabEF.ConsoleApp.Data.FatturarioContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
