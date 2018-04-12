96c1e123c758ed63c038d32436eee84f1de62e12
Walter <walter.mongelluzzo@gmail.com>
2018-04-12 15:42:36 +0200

    Creato FatturarioContext e impostata la connessione

---
+++ LabEF/LabEF.ConsoleApp/Data/FatturarioContext.cs
@@ -0,0 +1,16 @@
+﻿using LabEF.ConsoleApp.Models;
+using System.Data.Entity;
+
+namespace LabEF.ConsoleApp.Data
+{
+    class FatturarioContext: DbContext
+    {
+        public FatturarioContext(): base("FatturarioDbConnection")
+        {
+        }
+
+        public DbSet<Fattura> Fatture { get; set; }
+
+        public DbSet<VoceFattura> VociFattura { get; set; }
+    }
+}
